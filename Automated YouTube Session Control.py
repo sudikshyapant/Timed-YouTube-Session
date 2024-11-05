f007447e75fada402f1dd41fc248361d318d8dd1
sudikshyapant <suizpant123@gmail.com>
2024-11-05 04:48:56 +0000

    nit: clean comments

--- Automated YouTube Session Control.py
+++ Automated YouTube Session Control.py
@@ -1,18 +1,20 @@
-from selenium import webdriver
+from selenium import webdriver 
 import time
     
 def close_youtube():
-    driver = webdriver.Chrome()  # You may need to download the Chrome WebDriver from https://sites.google.com/a/chromium.org/chromedriver/downloads
+    #Download the Chrome WebDriver from https://sites.google.com/a/chromium.org/chromedriver/downloads
+    driver = webdriver.Chrome() 
     driver.get("https://www.youtube.com/")
-    time.sleep(5)  # Wait for the page to load
+    time.sleep(5)  
 
     total_time = 0
     start_time = time.time()
 
-    while total_time <= 900:  # Limit total_time to 900 seconds i.e 15 minutes
+    #Limit total_time to 900 seconds i.e 15 minutes
+    while total_time <= 900:  
         total_time = time.time() - start_time
-       
-    if total_time > 900:  # If total_time exceeds 15 minutes, quit the driver
+    #Quit the driver, if total_time exceeds 15 minutes
+    if total_time > 900:  
         driver.quit()
 
 if __name__ == "__main__":
