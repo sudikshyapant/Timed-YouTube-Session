3c7d1461a021668300e6ac0452d28e595332362f
Sudikshya Pant <suizpant123@gmail.com>
2024-12-04 17:58:44 +0545

    chore(errorS): Improve error handling. Introduce logging

--- Automated YouTube Session Control.py
+++ Automated YouTube Session Control.py
@@ -1,21 +1,36 @@
 from selenium import webdriver 
 import time
+import logging
     
-def close_youtube():
-    #Download the Chrome WebDriver from https://sites.google.com/a/chromium.org/chromedriver/downloads
-    driver = webdriver.Chrome() 
-    driver.get("https://www.youtube.com/")
-    time.sleep(5)  
+def close_youtube(time_limit = 20):
+    
+    try:
+        logging.basicConfig(level = logging.INFO)
+        driver = webdriver.Chrome() 
+        driver.get("https://www.youtube.com/")
+        time.sleep(5)  
 
-    total_time = 0
-    start_time = time.time()
+        start_time = time.time()
 
-    #Limit total_time to 900 seconds i.e 15 minutes
-    while total_time <= 900:  
-        total_time = time.time() - start_time
-    #Quit the driver, if total_time exceeds 15 minutes
-    if total_time > 900:  
+        #Limit total_time to 900 seconds i.e 15 minutes
+        while True:  
+            current_elapsed__time = time.time() - start_time
+            logging.info(f"Current session time: {current_elapsed__time} seconds" )
+            time.sleep(10)
+            
+            if current_elapsed__time >= time_limit:
+                logging.warning("Time limit reached. Closing YouTube.")
+                break
+            
+            time.sleep(1)
+    
+    except Exception as e:
+        logging.info(f"An error occured: {e}")
+        
+    finally:
+        logging.info("Closing browser")
         driver.quit()
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     close_youtube()
